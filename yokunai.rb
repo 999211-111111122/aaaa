5c0be9cca6e2a0907e50973eb85584220ddf89e8
rukai-aoki <rukai_aoki@tbn.co.jp>
2020-03-07 18:57:53 +0900

    init

---
+++ yokunai.rb
@@ -0,0 +1,69 @@
+def nagai_method
+nagai_namae_nagasugite_konnnano_yomitakunai, tekitousugite_yokuwakaranakunaru_namae, konnnano_kaicha_damedayo_nagasugite_okorarerusi_minikui =
+1,
+"tekitouna_string",
+NazoKlass.new
+        if konnnano_kaicha_damedayo_nagasugite_okorarerusi_minikui.class == NazoClass
+          if konnnano_kaicha_damedayo_nagasugite_okorarerusi_minikui == 2
+            string = ""
+            string.push "k"
+            string.push "u"
+            string.push "s"
+            string.push "o"
+            string.push "c"
+            string.push "o"
+            string.push "d"
+            string.push "e"
+            string.push "o"
+            string.push "t"
+            string.push "s"
+            string.push "u"
+            string.push "2"
+            p string
+            p String.new
+          elsif konnnano_kaicha_damedayo_nagasugite_okorarerusi_minikui == 3
+            string = ""
+            string.push "k"
+            string.push "u"
+            string.push "s"
+            string.push "o"
+            string.push "c"
+            string.push "o"
+            string.push "d"
+            string.push "e"
+            string.push "o"
+            string.push "t"
+            string.push "s"
+            string.push "u"
+            string.push "3"
+            p string
+            p String.new
+          elsif konnnano_kaicha_damedayo_nagasugite_okorarerusi_minikui.is_a?(String)
+            string = ""
+            string.push "k"
+            string.push "u"
+            string.push "s"
+            string.push "o"
+            string.push "c"
+            string.push "o"
+            string.push "d"
+            string.push "e"
+            string.push "o"
+            string.push "t"
+            string.push "s"
+            string.push "u"
+            string.push "string"
+            p string
+            p String.new
+          elsif !!konnnano_kaicha_damedayo_nagasugite_okorarerusi_minikui
+            if !tekitousugite_yokuwakaranakunaru_namae.nil?
+              p "kusocodeotsu"
+            end
+          end
+        end
+
+  hokano_hash={nannde: "kokodeyarunnya"}
+  hokano_hash.each do ||
+
+  end
+end
